dce3a600512a0a126b48cb95e50f63e75e4a2baa
Han <hanky_chung@hotmail.com>
2018-07-02 18:04:52 +0800

    add randomSelect

--- src/main.js
+++ src/main.js
@@ -1,12 +1,32 @@
 import './style/index.styl'
 
+const POP_NUM = 2
 let cellArr = document.querySelectorAll('.cell')
-console.log(cellArr)
 let idArr = []
 for (let i = 0; i < cellArr.length; i++) {
   idArr.push(cellArr[i].id)
 }
-console.log(idArr)
+
+start()
+
+// 初始化
+function start() {
+  let startCells = randomSelect(idArr, 2)
+  startCells.forEach(item => {
+    document.getElementById(item).innerHTML = POP_NUM
+  })
+}
+
+// 从数组中随机抽取互不相同的几个元素(*性能最佳的方法*)
+function randomSelect(arr, num) {
+  let result = []
+  for (let i=0; i<num; i++) {
+    let index = Math.floor(Math.random() * (arr.length - i)) + i
+    result.push(arr[index])
+    arr[index] = arr[i]
+  }
+  return result
+}
 
 
 
