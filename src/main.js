b43342cbfc1b06f03320ec05b178ce6054ed0246
Han <hanky_chung@hotmail.com>
2018-07-05 21:26:38 +0800

    finish movedown logic

--- src/main.js
+++ src/main.js
@@ -31,72 +31,27 @@ function getDataCells() {
 
 
 
-
+// 执行函数
 start()
-
 getDom('down').addEventListener('click', moveDown)
+
 
 // 开始初始化
 function start() {
-  clear()
-  // 获取单元格的id数组
-  let idArr = []
-  for (let i = 0; i < cellArr.length; i++) {
-    idArr.push(cellArr[i].id)
-  }
-  // 从id数组中随机选取两个id，赋值
-  let startCells = randomSelect(idArr, 2)
-  startCells.forEach(item => {
-    getDom(item).innerHTML = POP_NUM
-  })
+  clear()  
+  popNum(2)
 }
-
-
 
 // 向下
 function moveDown() { 
-  // 将有数据的格子归类到相对应的列中(存储的是dom的id) 
-  getDataCells()
-  let ogData = cols
-
+  // 将有数据的格子归类到相对应的列中(存储的是dom的id)   
+  getDataCells()  
+  let ogData = cols 
   // 暂存计算后的新数据(存储的是计算后的数值)
-  let temp = {
-    0: [],
-    1: [],
-    2: [],
-    3: []
-  }
-  for (let key in cols) {
-    let col = cols[key]
-    let len = col.length    
-    if (len > 1) {      
-      for (let i=0; i < len; i++) {  
-        if (getDom(col[i]).innerHTML == getDom(col[i+1]).innerHTML) {
-          temp[key].push(parseInt(getDom(col[i]).innerHTML)*2)
-          i++
-          // 如果当前数据是倒数第二个数，则直接push最后一个数值，并返回
-          if (i == len-2) {
-            temp[key].push(parseInt(getDom(col[i+1]).innerHTML))
-            return
-          }
-        } else {
-          temp[key].push(parseInt(getDom(col[i]).innerHTML))
-          // 如果当前数据是倒数第二个数，则直接push最后一个数值，并返回
-          if (i == len-2) {
-            temp[key].push(parseInt(getDom(col[i+1]).innerHTML))
-            return
-          }
-        }
-      }
-    } else if (len > 0) {
-      temp[key].push(parseInt(getDom(col[0]).innerHTML))
-    }       
-  }
+  let temp = calData(cols)  
   console.log(temp) 
-
   // 清除原数据
   clear()
-
   // 渲染新数据
   for (let key in temp) {
     if (temp[key].length) {
@@ -108,17 +63,15 @@ function moveDown() {
       }
     }
   }
-
   getDataCells()
   let newData = cols
-
   // 无变化则不做任何操作
   if (ogData === newData) {
+    console.log('no move')
     return
   }
-
-  
-  
+  // 在空位生成两个新元素
+  setTimeout(popNum(2), 20)   
 }
 
 // 从数组中随机抽取互不相同的几个元素(*性能最佳的方法*)
@@ -139,6 +92,60 @@ function clear() {
   }
 }
 
+// 在空位随机生成n个数据
+function popNum(num) {
+  let idArr = []
+  // 找出现存的空格子，将空格子id收集  
+  for (let i = 0; i < cellArr.length; i++) {    
+    if (!cellArr[i].innerHTML) {
+      idArr.push(cellArr[i].id)
+    }   
+  }  
+  // 从id数组中随机选取n个id，赋值
+  let popCells = randomSelect(idArr, num)  
+  popCells.forEach(item => {
+    getDom(item).innerHTML = POP_NUM
+  })
+}
+
+// 计算结果并返回结果列表
+function calData(cols) {
+  let temp = {
+    0: [],
+    1: [],
+    2: [],
+    3: []
+  }
+  for (let key in cols) {
+    let col = cols[key]
+    let len = col.length    
+    if (len > 1) {      
+      for (let i=0; i < len; i++) {  
+        if (getDom(col[i]).innerHTML == getDom(col[i+1]).innerHTML) {
+          temp[key].push(parseInt(getDom(col[i]).innerHTML)*2)
+          i++
+          // 如果当前数据是倒数第二个数，则直接push最后一个数值，并结束循环
+          if (i == len-2) {
+            temp[key].push(parseInt(getDom(col[i+1]).innerHTML))
+            break
+          }
+        } else {
+          temp[key].push(parseInt(getDom(col[i]).innerHTML))
+          // 如果当前数据是倒数第二个数，则直接push最后一个数值，并结束循环
+          if (i == len-2) {
+            temp[key].push(parseInt(getDom(col[i+1]).innerHTML))
+            break
+          }
+        }
+      }
+    } else if (len > 0) {
+      temp[key].push(parseInt(getDom(col[0]).innerHTML))
+    }       
+  }
+  return temp
+}
+
+// 简化
 function getDom(id) {
   return document.getElementById(id)
 }
